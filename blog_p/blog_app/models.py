778de15a7467d8a66893e40fe3f948e68de819bc
Myiah Moore <myiahsade@gmail.com>
2018-10-18 09:47:06 -0500

    check in

--- blog_p/blog_app/models.py
+++ blog_p/blog_app/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
+
 
 class BlogPost(models.Model):
-    username = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, default='name')
     blogTitle = models.CharField(max_length=200)
     blogEntry = models.CharField(max_length=500)
-    DateItWasCreated =models.DateField()
+    DateItWasCreated =models.DateTimeField(default=datetime.now)
+    username = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
--- blog_p/blog_app/views.py
+++ blog_p/blog_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from .models import BlogPost
+
+
+def index(request):
+    form_list = BlogPost.objects.all()
+    context = {'form_list': form_list}
+    return render(request, 'blog_app/index.html', context)
+
+def userIndex(request):
+    form_list = BlogPost.objects.filter(username= request.user)
+    context = {'form_list': form_list}
+    return render(request, 'blog_app/index.html', context)
